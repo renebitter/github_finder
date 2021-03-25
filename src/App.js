1a5f8b5cc6b46ee5e874ee0dfcc9c15651aab854
Rene <renebitter@gmail.com>
2021-03-25 10:52:46 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  foo = () => "bars"; //Method (needs "this" to reference it)
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <div className="container">
+          <Users />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
