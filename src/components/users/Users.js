af78efcd93f13af26f84cc22b33bd57ed58dfbd3
Rene <renebitter@gmail.com>
2021-03-25 16:37:07 -0300

    Added axios and State Update

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,34 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 export class Users extends Component {
-    state = {
-        users: [
-            {
-                id: '1',
-                login: 'mojombo',
-                avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-                html_url: 'https://github.com/mojombo'
-            },
-            {
-                id: '2',
-                login: 'defunkt',
-                avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-                html_url: 'https://github.com/defunkt'
-            },
-            {
-                id: '3',
-                login: 'pjhyett',
-                avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-                html_url: 'https://github.com/pjhyett'
-            }
-        ]
-
-    }
-
     render() {
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => {
+                {this.props.users.map(user => {
                     return < UserItem key={user.id} user={user} />
                 })}
             </div>
