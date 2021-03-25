db2c03828a285c43635279a91be1812188a3b28c
Rene <renebitter@gmail.com>
2021-03-25 11:23:29 -0300

    Refactor class components to function components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class UserItem extends Component {
-    render() {
-        const { login, avatar_url, html_url } = this.props.user;
+const UserItem = (props) => {
+    const { login, avatar_url, html_url } = props.user;
 
-        return (
-            <div className="card text-center">
-                <img className="round-img" style={{ width: '60px' }} src={avatar_url} alt={login} />
-                <h3>{login}</h3>
-                <div>
-                    <a className="btn btn-dark bt-sm my-1" href={html_url}>More</a>
-                </div>
+    return (
+        <div className="card text-center">
+            <img className="round-img" style={{ width: '60px' }} src={avatar_url} alt={login} />
+            <h3>{login}</h3>
+            <div>
+                <a className="btn btn-dark bt-sm my-1" href={html_url}>More</a>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default UserItem
