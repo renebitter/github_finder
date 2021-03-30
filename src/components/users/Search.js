8d59a536864ba6d91879578c5e5664fd04032cf7
Rene <renebitter@gmail.com>
2021-03-30 10:39:38 -0300

    Added Router and about page

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -16,7 +16,7 @@ export class Search extends Component {
   onSubmit = (e) => {
     e.preventDefault();
     if (this.state.text === '') {
-      this.props.setAlert('Please enter something', 'light');
+      this.props.setAlert('Please enter something', 'primary');
     } else {
       this.props.searchUsers(this.state.text);
       this.setState({ text: '' });
