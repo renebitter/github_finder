1ca8c5488b21d15d36abd497549b95455a7f92e8
Rene <renebitter@gmail.com>
2021-04-01 15:33:56 -0300

    Added loading spinner, back btn and prop types

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+  };
 
   render() {
     const {
@@ -23,7 +32,16 @@ export class User extends Component {
 
     const { loading } = this.props;
 
-    return <div>{name}</div>;
+    if (loading) return <Spinner />;
+
+    return (
+      <Fragment>
+        <h1>{name}</h1>
+        <Link to='/' className='btn btn-light'>
+          Back to search
+        </Link>
+      </Fragment>
+    );
   }
 }
 
