07b72e715287163e76a74ed8d20740f6743bd748
rene <renebitter@gmail.com>
2021-04-01 23:25:43 -0300

    Added profile info

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -27,7 +27,8 @@ export class User extends Component {
       following,
       public_repos,
       public_gists,
-      hireble,
+      hireable,
+      company,
     } = this.props.user;
 
     const { loading } = this.props;
@@ -36,10 +37,70 @@ export class User extends Component {
 
     return (
       <Fragment>
-        <h1>{name}</h1>
         <Link to='/' className='btn btn-light'>
           Back to search
         </Link>
+        Hireable:
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              alt={name}
+              className='round-img'
+              style={{ width: '150px' }}
+            />
+            <h2>{name}</h2>
+            {location && <p>Location: {location}</p>}
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong>{' '}
+                    <a href={blog} target='blank' rel='noopener noreferrer'>
+                      {blog}
+                    </a>
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+        </div>
       </Fragment>
     );
   }
