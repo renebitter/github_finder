030b3a6cea6d1ca96eaf248ee2297a4b0816c124
rene <renebitter@gmail.com>
2021-04-07 15:58:46 -0300

    Create Reducer & Actions

--- src/App.js
+++ src/App.js
@@ -38,18 +38,6 @@ const App = () => {
   }, []);
 
   // Search Github users
-  const searchUsers = async (text) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&
-      client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
 
   // Get single Github user
   const getUser = async (username) => {
@@ -105,7 +93,6 @@ const App = () => {
                 render={(props) => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       showClear={users.length > 0 ? true : false}
                       setAlert={showAlert}
